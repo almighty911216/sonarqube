139909afae060d878d74abbbec0369891a702411
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-05-26 16:25:49 +0200

    SONAR-9245 Add all leak facets

--- server/sonar-web/src/main/js/apps/projects/components/PageSidebar.js
+++ server/sonar-web/src/main/js/apps/projects/components/PageSidebar.js
@@ -22,9 +22,20 @@ import React from 'react';
 import { Link } from 'react-router';
 import FavoriteFilterContainer from './FavoriteFilterContainer';
 import LanguagesFilterContainer from '../filters/LanguagesFilterContainer';
-import PageSidebarOverall from './PageSidebarOverall';
+import CoverageFilter from '../filters/CoverageFilter';
+import DuplicationsFilter from '../filters/DuplicationsFilter';
+import MaintainabilityFilter from '../filters/MaintainabilityFilter';
+import NewCoverageFilter from '../filters/NewCoverageFilter';
+import NewDuplicationsFilter from '../filters/NewDuplicationsFilter';
+import NewMaintainabilityFilter from '../filters/NewMaintainabilityFilter';
+import NewReliabilityFilter from '../filters/NewReliabilityFilter';
+import NewSecurityFilter from '../filters/NewSecurityFilter';
+import NewSizeFilter from '../filters/NewSizeFilter';
 import QualityGateFilter from '../filters/QualityGateFilter';
+import ReliabilityFilter from '../filters/ReliabilityFilter';
+import SecurityFilter from '../filters/SecurityFilter';
 import SearchFilterContainer from '../filters/SearchFilterContainer';
+import SizeFilter from '../filters/SizeFilter';
 import TagsFilterContainer from '../filters/TagsFilterContainer';
 import { translate } from '../../../helpers/l10n';
 
@@ -75,8 +86,77 @@ export default function PageSidebar({
         <SearchFilterContainer query={query} isFavorite={isFavorite} organization={organization} />
       </div>
       <QualityGateFilter query={query} isFavorite={isFavorite} organization={organization} />
-      {!isLeakView &&
-        <PageSidebarOverall query={query} isFavorite={isFavorite} organization={organization} />}
+      {!isLeakView && [
+        <ReliabilityFilter
+          key="reliability"
+          query={query}
+          isFavorite={isFavorite}
+          organization={organization}
+        />,
+        <SecurityFilter
+          key="security"
+          query={query}
+          isFavorite={isFavorite}
+          organization={organization}
+        />,
+        <MaintainabilityFilter
+          key="maintainability"
+          query={query}
+          isFavorite={isFavorite}
+          organization={organization}
+        />,
+        <CoverageFilter
+          key="coverage"
+          query={query}
+          isFavorite={isFavorite}
+          organization={organization}
+        />,
+        <DuplicationsFilter
+          key="duplications"
+          query={query}
+          isFavorite={isFavorite}
+          organization={organization}
+        />,
+        <SizeFilter key="size" query={query} isFavorite={isFavorite} organization={organization} />
+      ]}
+      {isLeakView && [
+        <NewReliabilityFilter
+          key="new_reliability"
+          query={query}
+          isFavorite={isFavorite}
+          organization={organization}
+        />,
+        <NewSecurityFilter
+          key="new_security"
+          query={query}
+          isFavorite={isFavorite}
+          organization={organization}
+        />,
+        <NewMaintainabilityFilter
+          key="new_maintainability"
+          query={query}
+          isFavorite={isFavorite}
+          organization={organization}
+        />,
+        <NewCoverageFilter
+          key="new_coverage"
+          query={query}
+          isFavorite={isFavorite}
+          organization={organization}
+        />,
+        <NewDuplicationsFilter
+          key="new_duplications"
+          query={query}
+          isFavorite={isFavorite}
+          organization={organization}
+        />,
+        <NewSizeFilter
+          key="new_size"
+          query={query}
+          isFavorite={isFavorite}
+          organization={organization}
+        />
+      ]}
       <LanguagesFilterContainer query={query} isFavorite={isFavorite} organization={organization} />
       <TagsFilterContainer query={query} isFavorite={isFavorite} organization={organization} />
     </div>
